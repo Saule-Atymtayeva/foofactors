e5aecd82a72e13071d6a63473e38024df6dae0af
Saule Atymtayeva <sa080388@student.ubc.ca>
2021-03-04 19:48:35 -0800

    Add testing infrastructure

--- R/fbind.R
+++ R/fbind.R
@@ -3,10 +3,10 @@
 #' Create a new factor from two existing factors, where the new factor's levels
 #' are the union of the levels of the input factors.
 #'
-#' @param a
-#' @param b
+#' @param a factor
+#' @param b factor
 #'
-#' @return
+#' @return factor
 #' @export
 #'
 #' @examples
